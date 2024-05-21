2f0e9e19615a8a338bce91d4fc36c3555487e9ba
Dustan Kraus <dustan.kraus@maxar.com>
2024-05-21 14:45:53 -0700

    map space space to file search not git file search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
     "nvim-telescope/telescope.nvim",
     keys = {
       {"<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find files in root dir"},
+      {"<leader><space>", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find files in root dir"},
       {"<leader>fg", "<cmd>Telescope git_files show_untracked=true<cr> ", desc = "Find git files in repo without submodules"},
     },
   },
