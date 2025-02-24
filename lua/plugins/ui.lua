722271122473148dd98bdcd764ac101f990d65f0
Dustan Kraus <dustan.kraus@gmail.com>
2025-02-24 08:52:30 -0700

    add what was on laptop

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,12 @@
 return {
   { "goolord/alpha-nvim", enabled = false },
+  -- {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   opts = {
+  --     window = {
+  --       width = 60, -- Only adjust the width without overriding other settings
+  --     },
+  --   },
+  -- },
 }
 
